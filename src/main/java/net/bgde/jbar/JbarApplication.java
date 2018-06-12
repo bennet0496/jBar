fe25cf7aa0cc9d70ac4971653815bbe997997e6b
Bennet Becker <bennet@becker-dd.de>
2018-06-12 21:46:49 +0200

    Inital Commit [SpringBoot]

---
+++ src/main/java/net/bgde/jbar/JbarApplication.java
@@ -0,0 +1,12 @@
+package net.bgde.jbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbarApplication.class, args);
+	}
+}
