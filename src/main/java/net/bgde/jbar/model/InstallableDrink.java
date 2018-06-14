2c5e8e94dc2ce9277996e3d88f02c5e4a3ab3417
richi9514 <40272743+richi9514@users.noreply.github.com>
2018-06-14 19:13:17 +0200

    InstallableDrink

---
+++ src/main/java/net/bgde/jbar/model/InstallableDrink.java
@@ -0,0 +1,10 @@
+package net.bgde.jbar.model;
+
+public class InstallableDrink {
+    private String name;
+    private DrinkType type;
+
+    public InstallableDrink(String name){
+        this.name = name;
+    }
+}
