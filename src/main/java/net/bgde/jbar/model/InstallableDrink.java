1976dab364183adad7c93d7dcd0dccd1c3e6c466
Bennet Becker <bennet@becker-dd.de>
2018-06-14 19:20:28 +0200

    update InstallableDrink

--- src/main/java/net/bgde/jbar/model/InstallableDrink.java
+++ src/main/java/net/bgde/jbar/model/InstallableDrink.java
@@ -1,10 +1,32 @@
 package net.bgde.jbar.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class InstallableDrink {
+    @Id @GeneratedValue
+    private Long id;
     private String name;
     private DrinkType type;
 
-    public InstallableDrink(String name){
+    public InstallableDrink(String name, DrinkType type){
         this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DrinkType getType() {
+        return type;
+    }
+
+    public void setType(DrinkType type) {
+        this.type = type;
     }
 }
