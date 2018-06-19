2888f0ceea32fb725076342fe550e53b7dd4f016
richi9514 <40272743+richi9514@users.noreply.github.com>
2018-06-19 17:42:49 +0200

    Ingredient

--- src/main/java/net/bgde/jbar/model/Ingredient.java
+++ src/main/java/net/bgde/jbar/model/Ingredient.java
@@ -1,9 +1,45 @@
 package net.bgde.jbar.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Ingredient {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
     private int  amount;
     private DrinkType type;
 
-    if (InstallableDrinkRepository.findAllByName (name) == )
+    public InstallableDrink(String name, int amount, DrinkType type){
+        this.name = name;
+        this.amount = amoount;
+        this.type = type;
+    }
+
+    public DrinkType getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setType(DrinkType type) {
+        this.type = type;
+    }
 }
