575899db7536bc2614815b5e8d82d506e8a803e4
richi9514 <40272743+richi9514@users.noreply.github.com>
2018-06-19 17:24:48 +0200

    InstallableDrinkRepository

---
+++ src/main/java/net/bgde/jbar/model/Ingredient.java
@@ -0,0 +1,9 @@
+package net.bgde.jbar.model;
+
+public class Ingredient {
+    private String name;
+    private int  amount;
+    private DrinkType type;
+
+    if (InstallableDrinkRepository.findAllByName (name) == )
+}
