cbe5b27faf4c5072e729ac881858fd9179301327
richi9514 <40272743+richi9514@users.noreply.github.com>
2018-06-19 17:53:38 +0200

    IngredientRepository

--- src/main/java/net/bgde/jbar/model/IngredientRepository.java
+++ src/main/java/net/bgde/jbar/model/IngredientRepository.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IngredientRepository extends CrudRepository<Ingredient, Long>{
-    Iterable<InstallableDrink> findAll();
+    Iterable<Ingredient> findAll();
 
-    Iterable<InstallableDrink> findAllByName(String name);
+    Iterable<Ingredient> findAllByName(String name);
 
-    Iterable<InstallableDrink> findAllByType(DrinkType type);
+    Iterable<Ingredient> findAllByType(DrinkType type);
 }
